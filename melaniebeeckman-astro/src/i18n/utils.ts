933846b484900cb3d611e53210747f9e9fd1f160
jenodekeyzer <jenodekeyzer@gmail.com>
2025-06-03 14:39:14 +0200

    Fix language routing and simplify language extraction using Astro.currentLocale

--- melaniebeeckman-astro/src/i18n/utils.ts
+++ melaniebeeckman-astro/src/i18n/utils.ts
@@ -1,4 +1,4 @@
-import { defaultLang, languages } from './languages.ts';
+import { languages, defaultLang as defaultLanguage } from './languages.ts';
 import { en } from '@i18n/translations/en.ts';
 import { nl } from '@i18n/translations/nl.ts';
 import { fr } from '@i18n/translations/fr.ts';
@@ -6,10 +6,12 @@ import { fr } from '@i18n/translations/fr.ts';
 export type Language = keyof typeof languages;
 const translationsMap = { en, nl, fr };
 
-export function getLangFromUrl(url: URL) {
-    const [, lang] = url.pathname.split('/');
-    if (lang in languages) return lang as Language;
-    return defaultLang;
+export function getAsLanguage(lang?: string, defaultLang: string = defaultLanguage) {
+    return (lang && Object.keys(languages).includes(lang) ? lang : defaultLang) as Language;
+}
+
+export function getLanguagePaths() {
+    return Object.keys(languages).map((lang) => ({ params: { lang } }));
 }
 
 export function translate(lang: Language, key: string): string | undefined {
